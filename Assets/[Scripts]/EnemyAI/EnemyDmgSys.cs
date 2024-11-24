ca3d7a60e5b6ad237a5f09146776b855749373f3
Eva Luna Álvarez <zap363@amerike.edu.mx>
2024-11-23 18:04:00 -0600

    Working on lifesys Sync

--- Assets/[Scripts]/EnemyAI/EnemyDmgSys.cs
+++ Assets/[Scripts]/EnemyAI/EnemyDmgSys.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
 using ProjectSaga;
 using UnityEngine;
 
-public class EnemyDmgSys : MonoBehaviour
+public class EnemyDmgSys : NetworkBehaviour
 {
     [Header("Object Dealer of Damage")]
     [SerializeField] private GameObject _dealerofDmg = default;
+
+    [Header("Life System")] 
+    private readonly SyncVar<int> _life = new SyncVar<int>();
     
-    [Header("Life System")]
-    [SerializeField] private int _life = default;
-    
+    [Server]
     public void RemovingLife(int amount)
     {
         for (int i = 0; i < amount; i++)
         {
-            _life--;
-            if (_life <= 0)
+            //_life--;
+            /*if (_life <= 0)
             {
                 GameManager.Instance.PlayerDeath();
-            }
+            }*/
+            //TODO: Discover how to work with syncvars so I can change the value of life so this code works
         }
     }
     
