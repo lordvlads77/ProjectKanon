c8af8e6d1511e695f1deb751773b5e131ea378a3
Eva Luna Álvarez <zap363@amerike.edu.mx>
2024-11-25 14:08:37 -0600

    DealDmg Var wont Sync/Assign

--- Assets/[Scripts]/EnemyAI/EnemyDmgSys.cs
+++ Assets/[Scripts]/EnemyAI/EnemyDmgSys.cs
@@ -7,31 +7,43 @@ using UnityEngine;
 
 public class EnemyDmgSys : NetworkBehaviour
 {
-    [Header("Object Dealer of Damage")]
-    [SerializeField] private GameObject _dealerofDmg = default;
+    [Header("Object Dealer of Damage")] 
+    private readonly SyncVar<GameObject> _dealerofDmg = new SyncVar<GameObject>();
 
     [Header("Life System")] 
     private readonly SyncVar<int> _life = new SyncVar<int>();
-    
-    [Server]
-    public void RemovingLife(int amount)
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        if (_dealerofDmg.Value == null)
+        {
+            _dealerofDmg.Value = GameObject.FindWithTag("Enemy");
+            Debug.Log("Dealer of Damage has been set to" + _dealerofDmg.Value);
+        }
+    }
+
+    [ServerRpc]
+    public void RemovingLifeServerRPC(int amount)
     {
         for (int i = 0; i < amount; i++)
         {
-            //_life--;
-            /*if (_life <= 0)
+            _life.Value--;
+            Debug.Log("Life has been removed " + _life.Value);
+            if (_life.Value <= 0)
             {
                 GameManager.Instance.PlayerDeath();
-            }*/
+            }
             //TODO: Discover how to work with syncvars so I can change the value of life so this code works
         }
     }
     
     private void OnTriggerEnter(Collider col)
     {
-        if (_dealerofDmg.CompareTag("DamageDealer"))
+        if (_dealerofDmg.Value.CompareTag("Enemy"))
         {
-            RemovingLife(1);
+            Debug.Log("Player has been hit");
+            RemovingLifeServerRPC(1);
         }
     }
 }
